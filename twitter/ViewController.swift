1b74bb3ecc5dac5bd078ecdf47b05c98fe47a981
Charles Merriam <charles.merriam@gmail.com>
2014-10-06 15:59:59 -0700

    Initial Commit

---
+++ twitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twitter
+//
+//  Created by Charles Merriam on 10/6/14.
+//  Copyright (c) 2014 Charles Merriam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
